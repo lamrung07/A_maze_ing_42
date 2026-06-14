4c882fa6871f3398c937adf767fb4e86795b6d42
lamrung07 <lamrung012345@gmail.com>
2026-06-14 08:29:10 +0200

    add config

--- config_parser.py
+++ config_parser.py
@@ -21,74 +21,91 @@ class ConfigParser:
 
     '''Get config file data and save config'''
     def parse_config(self) -> None:
+
+        # Parsing raw data from config file
         try:
             with open(self.filename, "r") as f:
                 for line in f:
                     line = line.strip()
 
                     # Skip empty and comment
-                    if not line or line.startswith("#"):
+                    if not line or line[0] == "#":
                         continue
-
+                    
                     # Partition the line with "="
                     key, _, value = line.partition("=")
 
                     # Check if key is valid or not
                     if key not in self.config:
-                        print("Error: Invalid key")
+                        print(f"Error!: Invalid key '{key}'")
                         sys.exit(1)
                     else:
                         self.config[key.strip()] = value.strip()
-            
-            #Validate and casting config data
-            self.check_valid_data()
 
             # Check missing value
             for key, value in self.config.items():
                 if value is None:
                     print(f"Missing {key} in config")
                     sys.exit(1)
+
         except OSError as e:
             print (f"OS ERROR! {e}")
         except Exception as e:
             print(f"ERROR! {e}")
             sys.exit(1)
 
+
     '''Validate and casting config data'''
     def check_valid_data(self) -> None:
-            # 1.Check dimension values
-            for key, val in self.config.items():
-                try:
-                    if key == "WIDTH":
-                        if (int(val) < 0):
-                            print("ERROR!: WIDTH value must be >= 0")
-                            sys.exit(1)
-                        self.config.update({key: int(val)})
-                    if key == "HEIGHT":
-                        if (int(val) < 0):
-                            print("ERROR!: HEIGHT value must be >= 0")
-                            sys.exit(1)
-                        self.config.update({key: int(val)})
-                except ValueError as e:
-                    print(f"ERROR! {e}")
+
+        # 1.Check dimension values
+        try:
+            key, val = "WIDTH", self.config["WIDTH"]
+            if (int(val) < 0):
+                print("ERROR!: WIDTH value must be >= 0")
+                sys.exit(1)
+            self.config[key] = int(val)
+            key, val = "HEIGHT", self.config["HEIGHT"]
+            if (int(val) < 0):
+                print("ERROR!: HEIGHT value must be >= 0")
+                sys.exit(1)
+            self.config[key] = int(val)
+        except ValueError as e:
+                print(f"ERROR! {e}")
             
-            # 2.Check ENTRY AND EXIT values
-            for key, val in self.config.items():
-                try:
-                    if key == "ENTRY" or key == "EXIT":
-                        coordinates = val.split(',')
-                        if len(coordinates) != 2:
-                            print("ERROR! Invalid ENTRY/EXIT input")
-                            sys.exit(1)
-                        x, y = int(coordinates[0]), int(coordinates[1])
-                    width, height = self.config["width"], self.config["height"]
-                    if x < 0 or x >= width:
-                        print("ERROR! Invalid ENTRY/EXIT input")
-                    if y < 0 or y >= height:
-                        print("ERROR! Invalid ENTRY/EXIT input")
-                    self.config.update({key, (int(x), int(y))})
-                except ValueError as e:
-                    print(f"ERROR! {e}")
+        # 2.Check ENTRY AND EXIT values
+        try:
+            x, y = 0, 0
+            width = self.config["WIDTH"]
+            height = self.config["HEIGHT"]
+            
+            # Check ENTRY value
+            key, val = "ENTRY", self.config["ENTRY"]
+            coordinates = val.split(',')
+            if len(coordinates) != 2:
+                print("ERROR! Invalid ENTRY input")
+                sys.exit(1)
+            x, y = int(coordinates[0]), int(coordinates[1])
+            if x < 0 or x >= width:
+                print("ERROR! Invalid ENTRY input")
+            if y < 0 or y >= height:
+                print("ERROR! Invalid ENTRY input")
+            self.config[key] = (int(x), int(y))
+                    
+            # Check EXIT value
+            key, val = "EXIT", self.config["EXIT"]
+            coordinates = val.split(',')
+            if len(coordinates) != 2:
+                print("ERROR! Invalid EEXIT input")
+                sys.exit(1)
+            x, y = int(coordinates[0]), int(coordinates[1])
+            if x < 0 or x >= width:
+                print("ERROR! Invalid EXIT input")
+            if y < 0 or y >= height:
+                print("ERROR! Invalid EXIT input")
+            self.config[key] = (int(x), int(y))
+        except ValueError as e:
+            print(f"ERROR! {e}")
 
             # 3. Check PERFECT bool value
             for key, val in self.config.items():
--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from config_parser import ConfigParser
 
 
 Maze_config = ConfigParser("config.txt")
-# Maze_config.parse_config()
-print(Maze_config.config)
+Maze_config.parse_config()
+Maze_config.check_valid_data()
+for key,val in Maze_config.config.items():
+    print(f"{key} : {val}")
 # print(Maze_config.get_value("WIDTH"))
