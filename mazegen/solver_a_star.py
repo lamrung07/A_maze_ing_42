e804c43f23cd9fa36fc7e3287bb421fc570afcb5
lamrung07 <lamrung012345@gmail.com>
2026-06-27 17:42:58 +0200

    27/6

--- mazegen/solver_a_star.py
+++ mazegen/solver_a_star.py
@@ -3,10 +3,6 @@ from maze_generator import MazeGenerator
 from collections import deque
 from constance import OPEN_DIRS
 
-class NodeA_star:
-    """A node class for A* path finding"""
-    def __init__(self):
-        pass
 class MazeSolverA_star:
     def __init__(
         self,
@@ -22,7 +18,50 @@ class MazeSolverA_star:
         self.width = width
         self.height = height
         self.solution_path: str = ""
-    
+
     def solver_a_star(self):
+        """
+        Add path contain 'N','E','S','W' to self.solution_path
+        which represent the shortest path from entry to exit
+        """
         entry_x, entry_y = self.entry
         exit_x, exit_y = self.exit
+        entry = (entry_x, entry_y)
+        exit = (exit_x, exit_y)
+        def h(current: set[int, int] , target: set[int, int]) -> int:
+            """Caculate Manhattan distance from current to target node"""
+            return abs(current[0] - target[0]) + abs(current[1] - target[1])
+        # open_list: a list of (f, g, position)
+        open_list: list[int, int, set[int, int]] = [h(entry, exit), 0, entry]
+        g_cost = {entry: 0}
+        came_from = {}
+        directions = OPEN_DIRS
+        visited: set[tuple[int, int]] = {(entry_x, entry_y)}
+
+        while open_list:
+            f, g, current = min(open_list, key=lambda x: x[0])
+            open_list.remove((f, g, current))
+
+            if current == exit:
+                path = []
+                while current in came_from:
+                    path.append(current)
+                    current = came_from[current]
+                path.append(entry)
+                return path[::-1]
+
+            # Check all open neighbor of current cell
+            for dir, d_x, d_y, cur in directions:
+                current_val = self.maze[current[1]][current[0]]
+                neighbor_x = current[0] + d_x
+                neighbor_y = current[1] + d_y
+                neighbor = (neighbor_x, neighbor_y)
+                # Ingest neighbor cell if there is no wall
+                if (current_val & cur == 0
+                and (neighbor_x, neighbor_y) not in visited):
+                    new_g = g + 1
+                    if new_g < g_cost.get(neighbor, float('inf')):
+                        g_cost[neighbor] = new_g
+                        came_from[neighbor] = current
+                        f = new_g + h(neighbor, end)
+                        open_list.append((f, new_g, neighbor))
